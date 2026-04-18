b4890d79588eac29f9a45c8bb11a14b016288d78
michael-masarik <masarikfamilymichael@gmail.com>
2026-04-18 15:49:02 -0500

    Running process tests

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const USER_MAP = {
   [process.env.ED_EMAIL]: "ed",
   [process.env.DIANE_EMAIL]: "diane",
   [process.env.SARA_EMAIL]: "sara",
-  "masarikfamilymichael@gmail.com": "ed",
+  "masarikfamilymichael@gmail.com": "diane",
 };
 
 // Middleware to extract user from header
